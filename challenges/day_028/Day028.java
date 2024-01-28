09bda7c8d16dfa7974db3dfac6fc4e15d68081c2
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-01-28 10:20:34 -0300

    day #28 complete

---
+++ challenges/day_028/Day028.java
@@ -0,0 +1,26 @@
+package challenges.day_028;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day028 {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa("João", 20));
+        pessoas.add(new Pessoa("Maria", 30));
+        pessoas.add(new Pessoa("José", 22));
+
+        pessoas.sort((Pessoa p1, Pessoa p2) -> Integer.compare(p1.idade, p2.idade));
+        pessoas.forEach((p) -> System.out.println(p.nome + " - " + p.idade));
+    }
+}
+class Pessoa {
+
+    public String nome;
+    public int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
